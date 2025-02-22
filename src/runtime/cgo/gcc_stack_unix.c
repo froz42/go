82c7d8372b7cec9e1bdc736aadecfd0e69bd0975
Froz <froz@42paris.fr>
2025-02-22 11:43:42 +0100

    runtime/cgo: add error checks for safe stack retrieval on Unix

--- src/runtime/cgo/gcc_stack_unix.c
+++ src/runtime/cgo/gcc_stack_unix.c
@@ -8,6 +8,8 @@
 #define _GNU_SOURCE
 #endif
 
+#include <errno.h>
+#include <string.h>
 #include <pthread.h>
 #include "libcgo.h"
 
@@ -20,16 +22,21 @@ x_cgo_getstackbound(uintptr bounds[2])
 
 	// Needed before pthread_getattr_np, too, since before glibc 2.32
 	// it did not call pthread_attr_init in all cases (see #65625).
-	pthread_attr_init(&attr);
+	if (pthread_attr_init(&attr) != 0)
+		fatalf("pthread_attr_init failed: %s", strerror(errno));
 #if defined(__GLIBC__) || defined(__BIONIC__) || (defined(__sun) && !defined(__illumos__))
 	// pthread_getattr_np is a GNU extension supported in glibc.
 	// Solaris is not glibc but does support pthread_getattr_np
 	// (and the fallback doesn't work...). Illumos does not.
-	pthread_getattr_np(pthread_self(), &attr);  // GNU extension
-	pthread_attr_getstack(&attr, &addr, &size); // low address
+	if (pthread_getattr_np(pthread_self(), &attr) != 0) // GNU extension
+		fatalf("pthread_getattr_np failed: %s", strerror(errno));
+	if (pthread_attr_getstack(&attr, &addr, &size) != 0) // low address
+		fatalf("pthread_attr_getstack failed: %s", strerror(errno));
 #elif defined(__illumos__)
-	pthread_attr_get_np(pthread_self(), &attr);
-	pthread_attr_getstack(&attr, &addr, &size); // low address
+	if (pthread_attr_get_np(pthread_self(), &attr) != 0) // Illumos extension
+		fatalf("pthread_attr_get_np failed: %s", strerror(errno));
+	if (pthread_attr_getstack(&attr, &addr, &size) != 0) // low address
+		fatalf("pthread_attr_getstack failed: %s", strerror(errno));
 #else
 	// We don't know how to get the current stacks, leave it as
 	// 0 and the caller will use an estimate based on the current
